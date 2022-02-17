e48d6266457141071e866ca63dfbe35143a79000
Deby1997 <debysarker159@gmail.com>
2022-02-17 12:44:22 +0600

    second-commit

--- JS/calculation.js
+++ JS/calculation.js
@@ -1,10 +1,16 @@
-function getInput() {
-    const income = document.getElementById('income');
-    const incomeTaka = income.value;
+
+function sum() {
+    var income = document.getElementById('income').value;
+    var food = document.getElementById('food').value;
+    var rent = document.getElementById('rent').value;
+    var clothes = document.getElementById('clothes').value;
+    var total = parseInt(food) + parseInt(rent) + parseInt(clothes);
+    document.getElementById('total-expenses').innerText = total;
+    document.getElementById('balance').innerText = parseInt(income) - total;
 
 }
 
 document.getElementById('calculate-button').addEventListener('click', function () {
 
-
+    sum();
 });
