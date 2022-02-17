0c01d8e16c29388f9c3ef4338b7e5cb866103475
Deby1997 <debysarker159@gmail.com>
2022-02-17 16:52:24 +0600

    fifth-commit

--- JS/calculation.js
+++ JS/calculation.js
@@ -8,16 +8,25 @@ function sum() {
 
 
     var fail = document.getElementById('notify-fail');
-    if (income > total) {
+    var failSecond = document.getElementById('notify-fail-two');
+
+    if (income < 0 || food < 0 || rent < 0 || clothes < 0) {
+        failSecond.style.display = 'block';
+    }
+    else if (income > total) {
         document.getElementById('total-expenses').innerText = parseInt(total);
         document.getElementById('balance').innerText = parseInt(income) - parseInt(total);
-
+        failSecond.style.display = 'none';
+        fail.style.display = 'none';
     }
+
     else {
         fail.style.display = 'block';
     }
-
-
+    // empty input values
+    document.getElementById('food').value = '';
+    document.getElementById('rent').value = '';
+    document.getElementById('clothes').value = '';
 }
 
 document.getElementById('calculate-button').addEventListener('click', function () {
@@ -34,6 +43,9 @@ function save() {
     var balance = document.getElementById('balance').innerText;
     var save = document.getElementById('saving-amount').innerText;
     document.getElementById('remaining-balance').innerText = parseFloat(balance) - parseFloat(save);
+    // empty input values
+    document.getElementById('income').value = '';
+    document.getElementById('save-input').value = '';
 }
 document.getElementById('save-button').addEventListener('click', function () {
 
