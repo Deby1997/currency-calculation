a86ea78ac8290a47bfcef0bd07a0ac95cb236515
Deby1997 <debysarker159@gmail.com>
2022-02-17 11:56:36 +0600

    first commit

---
+++ JS/calculation.js
@@ -0,0 +1,10 @@
+function getInput() {
+    const income = document.getElementById('income');
+    const incomeTaka = income.value;
+
+}
+
+document.getElementById('calculate-button').addEventListener('click', function () {
+
+
+});
